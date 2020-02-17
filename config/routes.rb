162605e27f423b7d1dec76f50906271b971a6bb2
Takahashi Takumu <takumu.0213@icloud.com>
2020-02-17 16:45:26 +0900

    [Add] Model,Controller,Routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'books' => 'books#index'
+  post 'books' => 'books#create'
+  get 'books/new' => 'books#new', as: 'new_book'
+  get 'books/:id/edit' => 'books#edit', as: 'edit_book'
+  get 'books/:id' => 'books#show', as: 'book'
+  patch 'books/:id' => 'books#update'
+  delete 'books/:id' => 'books#destroy'
 end
