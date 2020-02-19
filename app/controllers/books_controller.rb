11ff4532d0414484e310c081c4d259f8f114f185
Takahashi Takumu <takumu.0213@icloud.com>
2020-02-19 16:32:54 +0900

    [Finish] 4-8

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,7 @@
 class BooksController < ApplicationController
+  def top
+  end
+
   def index
     # 投稿一覧部分
     @books = Book.all
@@ -7,13 +10,12 @@ class BooksController < ApplicationController
   end
   
   def create
-    book = Book.new(book_params)
-    if book.save
+    @book = Book.new(book_params)
+    if @book.save
       flash[:notice] = 'Book was successfully created.'
-      redirect_to book_path(book)
+      redirect_to book_path(@book)
     else
       @books = Book.all
-      @book = Book.new
       render :index
     end
   end
@@ -27,12 +29,11 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    if book.update(book_params)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
       flash[:notice] = 'Book was successfully updated.'
-      redirect_to book_path(book)
+      redirect_to book_path(@book)
     else
-      @book = Book.find(params[:id])
       render :edit
     end
   end
@@ -41,7 +42,7 @@ class BooksController < ApplicationController
     book = Book.find(params[:id])
     book.destroy
     flash[:notice] = 'Book was successfully destroyed.'
-    redirect_to root_path
+    redirect_to books_path
   end
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-      root to: 'books#index'
-  post '/' => 'books#create'
-  get '/:id/edit' => 'books#edit', as: 'edit_book'
-  get '/:id' => 'books#show', as: 'book'
-  patch '/:id' => 'books#update'
-  delete '/:id' => 'books#destroy'
+  root :to => 'books#top'
+
+  resources :books
 end
